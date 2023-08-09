5604b210654c57d2b8d531a4e2b3d39e881a3c24
Alex Kim <alexkim@Alexs-MBP.lan>
2023-08-09 01:13:51 -0500

    Update

--- PiorArtSearch.py
+++ PiorArtSearch.py
@@ -6,9 +6,21 @@ from langchain.vectorstores import Chroma
 from docload import dataset_dict
 
 
+def retrieve(prompt, db):
+    retrieval = db.similarity_search(prompt, k =3)
+    return retrieval
 
+def retrieval2text(collections):
+    text = ""
+    for doc in collections:
+        content = doc.page_content
+        text += "\n" + content
+    return text
 
+#Text문서를 Embedding Vector로 변환시켜줄 모델 
 embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+#미국 특허 예제 문서를 Split
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000)
 patent_samples = dataset_dict["train"][:100]["claims"]
 
@@ -17,12 +29,9 @@ for sample in patent_samples:
     split_text = text_splitter.split_text(sample)
     list_texts.extend(split_text)
 
+#Chroma DB에 추출할때 사용할 Embedding Model과 Split된 문서 데이터 업로드
+us_patent = Chroma.from_texts(list_texts, embedding_model)
 
-db = Chroma.from_texts(list_texts, embedding_model)
-
-def retrieve(prompt):
-    retrieval = db.similarity_search(prompt, k =3)
-    return retrieval
 
 
 
